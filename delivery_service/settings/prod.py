503a714aad9a92cea1ece05d56ca6f9f22a38e87
glor1ee <sashok1764@icloud.com>
2026-07-31 15:43:06 +0300

    Log unhandled errors to console in production

--- delivery_service/settings/prod.py
+++ delivery_service/settings/prod.py
@@ -29,3 +29,15 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
